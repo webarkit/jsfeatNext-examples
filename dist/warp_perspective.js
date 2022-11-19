dbb1090ec5b5637793e200969ccdd24c8be827c6
kalwalt <info@kalwaltart.it>
2022-11-19 21:47:52 +0100

    new warp perspective example

---
+++ dist/warp_perspective.js
@@ -0,0 +1,62 @@
+import jsfeatNext from '@webarkit/jsfeat-next';
+import { VideoStream } from './VideoStream/VideoStream';
+console.log(jsfeatNext);
+const jsfeat = jsfeatNext.jsfeatNext;
+const U8_t = jsfeat.U8_t;
+const C1_t = jsfeat.C1_t;
+const F32_t = jsfeat.F32_t;
+let imgproc = new jsfeat.imgproc();
+let transform_c = new jsfeat.transform();
+let matmath = new jsfeat.matmath();
+let image_data;
+const videoSettings = {
+    width: {
+        min: 640,
+        max: 800
+    },
+    height: {
+        min: 480,
+        max: 600
+    },
+    facingMode: 'environment',
+    targetFrameRate: 30
+};
+var canvas = document.getElementById('canvas');
+var video = document.getElementById('video');
+const videoStream = new VideoStream(video);
+function render_mono_image(src, dst) {
+    var i = src.cols * src.rows, pix = 0;
+    var alpha = (0xff << 24);
+    while (--i >= 0) {
+        pix = src.data[i];
+        dst[i] = alpha | (pix << 16) | (pix << 8) | pix;
+    }
+}
+async function init() {
+    let stream = await videoStream.initialize(videoSettings);
+    return stream;
+}
+init().then(() => {
+    console.log('starting the app');
+    canvas.width = videoStream.width;
+    canvas.height = videoStream.height;
+    process();
+});
+let process = () => {
+    image_data = videoStream.image;
+    var width = 640, height = 480;
+    var img_u8, img_u8_warp, transform;
+    img_u8 = new jsfeat.matrix_t(width, height, U8_t | C1_t);
+    imgproc.grayscale(image_data.data, width, height, img_u8);
+    img_u8_warp = new jsfeat.matrix_t(640, 480, U8_t | C1_t);
+    transform = new jsfeat.matrix_t(3, 3, F32_t | C1_t);
+    transform_c.perspective_4point_transform(transform, 0, 0, 50, 50, 640, 0, 550, 100, 640, 480, 300, 400, 0, 480, 100, 400);
+    matmath.invert_3x3(transform, transform);
+    imgproc.warp_perspective(img_u8, img_u8_warp, transform, 0);
+    var data_u32 = new Uint32Array(image_data.data.buffer);
+    render_mono_image(img_u8_warp, data_u32);
+    var ctx = videoStream.contextProcess;
+    ctx.putImageData(image_data, 0, 0);
+    requestAnimationFrame(process);
+};
+//# sourceMappingURL=warp_perspective.js.map
